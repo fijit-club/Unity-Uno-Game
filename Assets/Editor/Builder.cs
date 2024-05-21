1b02a6c06faa5dfd7091083ed760678995174e93
TusharVaid30 <tusharvaid30@gmail.com>
2024-05-21 18:56:21 +0530

    builder

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -7,7 +7,7 @@ class Builder{
     {
 
         // Place all your scenes here
-        string[] scenes = {"Assets/Tetris Template/Scenes/GamePlay.unity"};
+        string[] scenes = {"Assets/Scenes/Start.unity"};
 
         string[] args = Environment.GetCommandLineArgs();
         string outputPath = "builds/raw/web"; // Set a default path if the argument is not provided
